9b6a12639bf83bd491a6ad6854748211a989ef9c
Paulhenrisolat <psolat@gaming.tech>
2025-04-13 20:25:33 +0200

    Update Projet2-TestSword.py

--- Projet2-TestSword.py
+++ Projet2-TestSword.py
@@ -89,6 +89,44 @@ def cylindre_plein(n, R, h):
         pts.append([x, y, z])
   return transpose(pts)
 
+def translation(m,F,G,vG,h):
+    SF=[0,0,0]
+    for i in range(len(F)):
+        SF=somme_vect(SF,F[i][0])
+        aG=prod_vect_scal(SF,1/m)
+        newG=somme_vect(G,prod_vect_scal(vG,h))
+        newvG=somme_vect(vG,prod_vect_scal(aG,h))
+    return(newG,newvG)
+
+
+# I = matrice inertie (3x3)
+# G = point de base de la matrice I
+# A = nouveau point ou la matrice sera déplacé (arrivée)
+# M = masse du solide
+# Dans la formule a=x b=y c=z
+
+def deplace_mat(I,m,G,A):
+  # G = [g₁, g₂, g₃] | A = [a₁, a₂, a₃]
+  # d = [g₁ - a₁, g₂ - a₂, g₃ - a₃]
+  d = []
+  for i in range(3):
+    diff = G[i] - A[i]
+    d.append(diff)
+  diagonal = d[0]**2 + d[1]**2 + d[2]**2
+
+  I_A = []
+  
+  for l in range(3):
+    new_ligne = []
+    for c in range(3):
+      # delta = 1 si on est sur la diagonale, sinon 0
+      delta = 1 if l == c else 0
+      # Application de la formule : I_A = I + m*(||d||² * delta - d[l]*d[c])
+      new_val = I[l][c] + m * (diagonal * delta - d[l] * d[c])
+      new_ligne.append(new_val)
+    I_A.append(new_ligne)
+
+  return I_A
 
 def translation(m, F, G, vG, h):
   new_g = 0
@@ -101,6 +139,11 @@ def rotation(I_inv,F,G,teta,omega,h):
   new_omega = 0
 
   return
+
+# test deplace mat
+I_test = np.array([[1, 0, 0],
+                   [0, 1, 0],
+                   [0, 0, 1]])
 
 # Tracé cylindre plein
 (X, Y, Z) = cylindre_plein(1000, 1, 9)
@@ -122,5 +165,24 @@ ax.set_title("The star sword !")
 
 ax.scatter3D(X_translated, Y_translated, Z_translated, c=Z_translated, cmap='autumn', s=5)
 ax.scatter3D(Xa, Ya, Za)
+
+I_test = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
+m_test = 10.0 #mass
+G_test = [1.0, 2.0, 3.0]
+A_test = [0.0, 0.0, 0.0]
+
+I_A_test = deplace_mat(I_test, m_test, G_test, A_test)
+
+print("Matrice d'inertie initiale I(G) :")
+for ligne in I_test:
+    print(ligne)
+
+print("\nMatrice d'inertie déplacée I(A) :")
+for ligne in I_A_test:
+    print(ligne)
 
 plt.show()
